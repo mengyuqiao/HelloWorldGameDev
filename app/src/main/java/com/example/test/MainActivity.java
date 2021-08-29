4a9c7da4dbe109c81c16208541ae64473b7850f9
mengyuqiao <1156253960@qq.com>
2021-08-28 22:02:30 -0400

    First Commit: create a hello-world app and create a button to change the text size

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textView = (TextView) findViewById(R.id.hello_text);
+        this.btn = (Button) findViewById(R.id.hello_btn);
+    }
+
+    public void OnClickBtn(View view){
+        this.textView.setTextSize(64);
     }
 }
